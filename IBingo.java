9e695a11bac0fb399b3453c1ef1ebb2d95366ac0
José Edson de Lucena Cisneiros Netto <josedsoncisneiros@gmail.com>
2025-11-12 10:36:28 -0300

    Delete IBingo.java

--- IBingo.java
+++
@@ -1,7 +0,0 @@
-public interface IBingo {
-    public Cartela gerarCartela(int quantidadeNumeros);
-    public int sortearNumero();
-    public void exibirBolasSorteadas();
-    public int contarNumerosSorteados();
-    public boolean checarSeUmNumeroJaFoiSorteado(int numero);
-}
